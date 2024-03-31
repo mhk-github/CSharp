4e61067214353fd29bb6b942c6d5e0780dc91edd
Mohammad Haroon <72883489+mhk-github@users.noreply.github.com>
2024-03-30 21:57:09 -0300

    Add files via upload

--- ProcessMaster/src/MainWindow.xaml.cs
+++ ProcessMaster/src/MainWindow.xaml.cs
@@ -19,6 +19,7 @@ using System.Diagnostics;
 using System.IO;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Threading;
 
 using log4net;
 
@@ -66,13 +67,6 @@ namespace ProcessMaster
          * </summary>
          */
         private readonly string[] _highProcessList;
-        
-        /**
-         * <summary>
-         * The number of milliseconds to wait before checking processes.
-         * </summary>
-         */
-        private readonly int _sleepTime;
 
         ////////////////////////////////////////////////////////////////////////
         // MEMBER FUNCTIONS
@@ -211,17 +205,12 @@ namespace ProcessMaster
             s_log.Info($"    Main window width = {mainWindowWidth}");
             s_log.Info($"    Main window height = {mainWindowHeight}");
             s_log.Info($"    Logging configuration = '{logCfg}'");
-            s_log.Info($"    Sleep time (ms) = '{sleepTime}'");
+            s_log.Info($"    Sleep time (ms) = {sleepTime}");
             s_log.Info($"    Idle process list: '{idleProcesses}'");
             s_log.Info($"    High process list: '{highProcesses}'");
 
             s_log.Debug("  Enter - MainWindow()");
             s_log.Debug("    'ProcessMaster' priority set to Idle");
-
-            // Set the member data
-            _idleProcessList = idleProcesses.Split('|');
-            _highProcessList = highProcesses.Split('|');
-            _sleepTime = sleepTime;
 
             // Set up the main window
 #if DEBUG
@@ -232,24 +221,63 @@ namespace ProcessMaster
             Top = mainWindowTopLeftY;
             Width = mainWindowWidth;
             Height = mainWindowHeight;
-
-            idleBox.Text = idleProcesses.Replace('|', '\n');
-            highBox.Text = highProcesses.Replace('|', '\n');
-
             s_log.Debug(
                 $"    Geometry set to dimensions {Width}x{Height} at "
                 + $"position ({Left},{Top})"
             );
 
-            CheckProcesses();
+            // Set the text blocks for processes
+            idleBox.Text = idleProcesses.Replace('|', '\n');
+            highBox.Text = highProcesses.Replace('|', '\n');
 
             // MHK: Note - This is dealing with a known issue in C# with WPF
             Dispatcher.ShutdownStarted += Window_Unloaded;
             s_log.Debug("    Registered 'Window_Unloaded' event handler");
 
+            // Set the member data
+            _idleProcessList = idleProcesses.Split('|');
+            _highProcessList = highProcesses.Split('|');
+
+            // Start the timer
+            var timer = new DispatcherTimer
+            {
+                Interval = TimeSpan.FromMilliseconds(sleepTime)
+            };
+            timer.Tick += Timer_Tick;
+            timer.Start();
+            s_log.Debug(
+                "    Registered 'Timer_Tick' event handler for timer events "
+                + $"each {sleepTime}ms"
+            );
+
             s_log.Debug("  Leave - MainWindow()");
         }
-        
+
+        /**
+         * <summary>
+         * Handler for the timer event. 
+         * </summary>
+         * 
+         * <param name="sender">
+         * Originator of this event.
+         * </param>
+         * 
+         * <param name="ea">
+         * Arguments connected to this event.
+         * </param>
+         */
+        private void Timer_Tick(object sender, EventArgs ea)
+        {
+            s_log.Debug($"  Enter - Timer_Tick({sender}, {ea})");
+            var result = Task.Run(() => CheckProcesses()).Result;
+            if (result != null)
+            {
+                statusBarText.Text = $"{DateTime.Now:HH:mm:ss}: {result}";
+
+            }
+            s_log.Debug("  Leave - Timer_Tick(...)");
+        }
+
         /**
          * <summary>
          * Handler for the Unloaded event.
@@ -267,7 +295,6 @@ namespace ProcessMaster
         {
             s_log.Debug($"  Enter - Window_Unloaded({sender}, {ea})");
             s_log.Debug("  Leave - Window_Unloaded(...)");
-            s_log.Debug("    Leave - CheckProcesses()");
             s_log.Info("ProcessMaster - End");
         }
 
@@ -275,38 +302,33 @@ namespace ProcessMaster
          * <summary>
          * Periodically checks processes and sets their priority class.
          * </summary>
+         * 
+         * <returns>
+         * A task string for one process with an updated priority or null.
+         * </returns>
          */
-        async private void CheckProcesses()
+        private async Task<string> CheckProcesses()
         {
             s_log.Debug("    Enter - CheckProcesses()");
 
-            while (true)
-            {
-                var taskIdle = Task.Run(
-                    () => 
-                    UpdateProcessPriorities(
-                        _idleProcessList,
-                        ProcessPriorityClass.Idle
-                    )
-                );
-                var taskHigh = Task.Run(
-                    () =>
-                    UpdateProcessPriorities(
-                        _highProcessList,
-                        ProcessPriorityClass.High
-                    )
-                );
-                var results = await Task.WhenAll(taskIdle, taskHigh);
-                var sbText = results[0] ?? results[1];
-                if (sbText != null)
-                {
-                    // Update status bar with any non-null message
-                    statusBarText.Text = (
-                        $"{DateTime.Now:HH:mm:ss}: {sbText}"
-                    );
-                }
-                await Task.Delay(_sleepTime);
-            }
+            var taskIdle = Task.Run(
+                () => 
+                UpdateProcessPriorities(
+                    _idleProcessList,
+                    ProcessPriorityClass.Idle
+                )
+            );
+            var taskHigh = Task.Run(
+                () =>
+                UpdateProcessPriorities(
+                    _highProcessList,
+                    ProcessPriorityClass.High
+                )
+            );
+            var results = await Task.WhenAll(taskIdle, taskHigh);
+
+            s_log.Debug("    Leave - CheckProcesses()");
+            return results[0] ?? results[1];
         }
     }
 }
